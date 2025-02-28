093c37b927a36ad4198b6d1d947d16e0188034b6
Junhyun Shin <hl1sqi@gmail.com>
2025-02-28 21:11:51 +0900

    separate plugins from vscode

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -61,7 +61,10 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
+  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins', cond = function()
+    return not vim.g.vscode
+  end },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
